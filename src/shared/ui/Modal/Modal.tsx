c0c50ef2618b04b8ef39ea0a61b1463fdade2b0d
Maksym Vorobyov <mycv2207@gmail.com>
2023-12-28 19:00:05 +0200

    fix theme

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -60,7 +60,6 @@ export const Modal = (props: ModalProps) => {
   const mods: Record<string, boolean> = {
     [cls.opened]: Boolean(isOpen),
     [cls.isClosing]: isClosing,
-    [cls[theme]]: true,
   };
 
   return (
