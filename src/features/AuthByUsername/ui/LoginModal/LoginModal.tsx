9d2cee9ced84a005196bd5a58923e3a996e42b66
Maksym Vorobyov <mycv2207@gmail.com>
2024-01-02 14:36:36 +0200

    add input

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { LoginForm } from '../LoginForm/LoginForm';
-import cls from './LoginModal.module.scss';
 
 interface LoginModalProps {
   className?: string;
@@ -14,7 +13,7 @@ export const LoginModal: FC<LoginModalProps> = (props) => {
   const { className = '', isOpen, onClose } = props;
 
   return (
-    <Modal className={classNames(cls.LoginModal, {}, [className])} onClose={onClose} isOpen={isOpen} lazy>
+    <Modal className={classNames(className, {}, [className])} onClose={onClose} isOpen={isOpen} lazy>
       <LoginForm />
     </Modal>
   );
